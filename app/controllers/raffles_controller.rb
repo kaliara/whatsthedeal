102d5f03d8bf688c6d11a80cc263d4ac8768d6b0
Matthew Burnett <matt@sowhatsthdeal.com>
2011-02-11 16:10:14 -0500

    updated raffle to hide enter functionality when over

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -25,7 +25,7 @@ class RafflesController < ApplicationController
     
     session[:stored_promotion_code_id] = nil
     
-    if @raffle.in_rotation? or params[:password] == Raffle::PREVIEW_PASSWORD
+    if @raffle.rotation_start_date <= Date.today or params[:password] == Raffle::PREVIEW_PASSWORD
       render :action => 'show'
     else
       flash[:error] = "Sorry, but that raffle is not available right now. Email us at support@sowhatsthedeal.com if you need help."
