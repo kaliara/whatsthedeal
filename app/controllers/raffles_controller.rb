4e654a011eeb5c8c16ac6975357835e8020f23ed
Matthew Burnett <matt@kaliara.com>
2011-02-21 21:39:46 -0500

    fixed raffle stuff

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -26,7 +26,7 @@ class RafflesController < ApplicationController
     session[:stored_promotion_code_id] = nil
     
     if @raffle.rotation_start_date <= Date.today or params[:password] == Raffle::PREVIEW_PASSWORD
-      flash.now[:error] = "This raffle has ended. Make sure you don't miss another raffle by <a href='/signup'>signing up for our Daily Deal</a>." if rotation_end_date > Date.today
+      flash.now[:error] = "This raffle has ended. Make sure you don't miss another raffle by <a href='/signup'>signing up for our Daily Deal</a>." if @raffle.rotation_end_date > Date.today
       render :action => 'show'
     else
       flash.now[:error] = "Sorry, but that raffle is not available right now. Email us at support@sowhatsthedeal.com if you need help."
