a69e4dd574284d98c8b70bb17c7074a59d7d2cea
Matthew Burnett <matt@sowhatsthdeal.com>
2011-03-28 08:03:21 -0400

    new credit report

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -15,10 +15,15 @@ class Admin::StatsController < ApplicationController
   end
   
   def monthly_credit
+    @start_date = Date.civil(2009, 8, 1)
+    @date = @start_date
     @credits = []
-    for i in 0..11 do
-      @credits << Credit.find(:all, :conditions => ['updated_at > ? and updated_at < ? and user_id is not null and purchase_id is not null and promotion_code_id != 2', Date.civil(Date.today.year,1,1)>>(i), Date.civil(Date.today.year,1,31)>>(i)]).collect{|c| c.value}.sum
+    while @date < Date.today
+      @credits << Credit.find(:all, :conditions => ['updated_at >= ? and updated_at < ? and user_id is not null and purchase_id is not null and promotion_code_id != 2', @date, @date + 1.month]).collect{|c| c.value}.sum
+      @date = @date + 1.month
     end
+    
+    @outstanding_credit = Credit.find(:all, :conditions => ['user_id is not null and promotion_code_id != 2 and purchase_id is null']).collect{|c| c.value}.sum
   end
   
   def zipcode_counts
