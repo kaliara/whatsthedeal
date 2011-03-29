4fb895adc5a76d052dbe19ccdefbae53744716bd
Matthew Burnett <matt@sowhatsthdeal.com>
2011-03-29 14:50:54 -0400

    added static action for nationals_deal and check for login

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,9 @@
 class StaticController < ApplicationController
   before_filter :impersonate_customer, :check_origin, :force_full_site
-  before_filter :customer_required, :only => ['totn_code']
+  before_filter :customer_required, :only => ['totn_code', 'nationals_deal']
   layout :hyrbrid_layout_application
 
-  @pages = %W(how_it_works faq info contact about suggest_business referral_info signup signup2 signup3 signup4 press survey biz special_event special_event2 special_event3 special_event4 special_event5 happyhour happyhour2 marchmadness raffle raffle2 raffle3 raffle4 raffle5 raffle6 raffle7 raffle8 raffle9 raffle10 raffle11 raffle12 raffle13 raffle13 raffle14 raffle15 rap mobile_info contact_hpdc faq_hpdc timeout_error totn_code)
+  @pages = %W(how_it_works faq info contact about suggest_business referral_info signup signup2 signup3 signup4 press survey biz special_event special_event2 special_event3 special_event4 special_event5 happyhour happyhour2 marchmadness raffle raffle2 raffle3 raffle4 raffle5 raffle6 raffle7 raffle8 raffle9 raffle10 raffle11 raffle12 raffle13 raffle13 raffle14 raffle15 rap mobile_info contact_hpdc faq_hpdc timeout_error totn_code nationals_deal)
   
   @pages.each do |page|
     define_method page do
