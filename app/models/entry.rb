aae13f657c82a332e452ab10901e3161821013d7
Matthew Burnett <matt@sowhatsthdeal.com>
2011-04-01 16:17:50 -0400

    uniquness for entries on raffles

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
   belongs_to :raffle
+
+  validates_uniqueness_of :user_id, :scope => :raffle_id, :on => :create, :message => "must be unique"
 end
