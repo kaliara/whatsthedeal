2df733cabb595a55bb491e7b2401907e58947399
Matthew Burnett <matt@sowhatsthdeal.com>
2011-04-07 18:08:40 -0400

    style updates for biz pages

--- app/controllers/business/purchases_controller.rb
+++ app/controllers/business/purchases_controller.rb
@@ -24,7 +24,7 @@ class Business::PurchasesController < ApplicationController
       @type = "Confirmation Code"
       @coupons = Coupon.find_by_confirmation_code(params[:q]).to_a
     elsif params[:q] =~ /\w+/
-      @type = "Last Name"
+      @type = "Name"
       @user_ids = Customer.find(:all, :conditions => ['last_name like ?', "%#{params[:q]}%"]).collect{|c| c.user.id}
       @coupons = Coupon.find(:all, :conditions => {:user_id => @user_ids, :deal_id => @deals}, :order => 'confirmation_code ASC')
     elsif params[:all] == 'yes'
@@ -54,13 +54,53 @@ class Business::PurchasesController < ApplicationController
       @deals = Promotion.find(:all, :conditions => {:business_id => @business_ids}, :order => 'id DESC').collect{|a|a.deals.collect{|d|d.id}}.flatten
     else
       @deals = [Promotion.find(:all, :conditions => {:business_id => @business_ids}, :order => 'id DESC').collect{|a|a.deals.collect{|d|d.id}}.flatten.first]
-      @default_promotion = Deal.find(@deals.first).promotion
     end
 
     @coupons = Coupon.find(:all, :conditions => ['deal_id in (?) and mobile_used = ? and redemption_date >= ? and redemption_date < ?', @deals.join(','), true, @start_date, @end_date + 1.day], :order => 'redemption_date ASC')
     
     render :action => 'mobile_redemptions'
   end
+  
+  
+  def stats
+    user = current_user.business? ? current_user : current_user.business_staff.business.user
+    @business_ids = Business.find(:all, :conditions => {:user_id => user.id}).collect{|b| b.id}
+    @promotions = Promotion.find(:all, :conditions => {:business_id => @business_ids})
+    
+    if params[:promotion_id].to_i > 0
+      @promotion = Promotion.find(params[:promotion_id])
+      @deals = Promotion.find(:all, :conditions => {:business_id => @business_ids, :id => params[:promotion_id].to_i}, :order => 'id DESC').collect{|a|a.deals.collect{|d|d.id}}.flatten
+      @coupons = Coupon.find(:all, :conditions => {:deal_id => @deals}, :order => 'created_at ASC')
+      @customers = Customer.find(:all, :conditions => {:user_id => @coupons.collect{|c|c.user_id}})
+      
+      @males = Customer.find(:all, :conditions => ["female = 0 and id in (#{@customers.collect{|c| c.id}.join(',')})"])
+      @females = Customer.find(:all, :conditions => ["female = 1 and id in (#{@customers.collect{|c| c.id}.join(',')})"])
+      @neither = Customer.find(:all, :conditions => ["female is null and id in (#{@customers.collect{|c| c.id}.join(',')})"])
+      
+      @zipcodes = @customers.collect{|c| c.zipcode}.delete_if{|z|z.blank?}.uniq
+      @zipcode_purchases = {}
+      @zipcodes.each do |zipcode|
+        @zipcode_purchases[zipcode] = Customer.find(:all, :conditions => {:id => @customers, :zipcode => zipcode}).size
+      end
+      @zipcode_purchases = @zipcode_purchases.sort {|a,b| b[1] <=> a[1]}
+    else
+      @deals = []
+    end
+
+    @line_items = []
+    
+    unless @deals.empty? or @promotion.nil?
+      @first_day = DateTime.new(@coupons.first.created_at.year,@coupons.first.created_at.month,@coupons.first.created_at.day)
+      @last_day  = DateTime.new(@coupons.last.created_at.year, @coupons.last.created_at.month, @coupons.last.created_at.day)
+      @days = (@last_day - @first_day)
+      for i in 0..@days+1 do
+        @line_items[i] = Coupon.find(:all, :conditions => ["deal_id in (#{@deals.join(',')}) and created_at >= ? and created_at < ?", (@first_day + i.days), (@first_day + (i+1).days)]).size
+      end
+    end
+
+    render :action => 'stats'
+  end
+  
 
   # GET /purchases/1
   # GET /purchases/1.xml
