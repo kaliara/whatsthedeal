4a923e8979a19898a79012f462913e10fabc39d7
Matthew Burnett <matt@kaliara.com>
2011-06-14 10:17:57 -0400

    fixed map

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -49,7 +49,7 @@ class PromotionsController < ApplicationController
   def grab_bag
     session[:force_full_site] = true
     
-    @promotions = Promotion.find(:all, :conditions => ['start_date < ? and end_date > ? and active = ? and grab_bag = ?', Time.now.utc, Time.now.utc, true, true], :order  => 'featured DESC, start_date DESC')
+    @promotions = Promotion.find(:all, :conditions => ['start_date < ? and end_date > ? and active = ? and grab_bag = ?', Time.now.utc, Time.now.utc, true, true], :order  => 'dc_featured DESC, start_date DESC')
     @side_promotions = Promotion.sidebar(@promotions.first.id)
 
     if @promotions.empty?
@@ -63,8 +63,8 @@ class PromotionsController < ApplicationController
   end
 
   def map
-    @mapped_promotions   = Promotion.find(:all, :conditions => ['start_date < ? and end_date > ? and active = ? and hidden = ? and physical_address = ?', Time.now.utc, Time.now.utc, true, false, true], :order  => 'featured DESC, start_date DESC')
-    @unmapped_promotions = Promotion.find(:all, :conditions => ['start_date < ? and end_date > ? and active = ? and hidden = ? and physical_address = ?', Time.now.utc, Time.now.utc, true, false, false], :order  => 'featured DESC, start_date DESC')
+    @mapped_promotions   = Promotion.find(:all, :conditions => ['start_date < ? and end_date > ? and active = ? and hidden = ? and physical_address = ?', Time.now.utc, Time.now.utc, true, false, true], :order  => 'dc_featured DESC, start_date DESC')
+    @unmapped_promotions = Promotion.find(:all, :conditions => ['start_date < ? and end_date > ? and active = ? and hidden = ? and physical_address = ?', Time.now.utc, Time.now.utc, true, false, false], :order  => 'dc_featured DESC, start_date DESC')
 
     @street  = params[:street]
     @city    = params[:city]
